1e9e809bf38185c1a7bc74e342ad9f0bf1684a6a
Alon Hershenhorn <hershen@gmail.com>
2021-07-25 23:35:25 -0700

    add print_readably to step1 2) catch ValueError used for unbalanced [ ( "

--- impls/myPython/step1_read_print.py
+++ impls/myPython/step1_read_print.py
@@ -7,10 +7,11 @@ def READ(line):
     return reader.read_str(str(line))
 
 def EVAL(mal_type):
+    # print(type(mal_type))
     return mal_type
 
 def PRINT(mal_type):
-    return printer.pr_str(mal_type)
+    return printer.pr_str(mal_type, print_readably=True)
 
 def rep(line):
     return PRINT(EVAL(READ(line)))
@@ -20,6 +21,9 @@ if __name__ == "__main__":
         try: 
             line = input('user> ')
             print(rep(line))
+        except ValueError as e:
+            print(e)
         except EOFError:
+            #EOF
             break
 
