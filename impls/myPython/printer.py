ff4be2e9482a6a8b460f8c2e5220bacd63303e71
Alon Hershenhorn <hershen@gmail.com>
2021-07-25 23:32:35 -0700

    Add print_readably prameter to print escape charecters

--- impls/myPython/printer.py
+++ impls/myPython/printer.py
@@ -1,3 +1,13 @@
+import mal_types
 
-def pr_str(mal_type):
-    return str(mal_type)
+def pr_str(mal_type, print_readably):
+    string_representation = str(mal_type)
+
+    # print(' before = ', string_representation, type(mal_type))
+    if print_readably and isinstance(mal_type, mal_types.String):
+        string_representation = '"' + string_representation[1:-1].replace('\\', '\\\\').replace('"', '\\"') + '"'
+            #                                                  .replace(')', '\\)')   \
+            #                                                  .replace('"', '\\"')   
+    # print(' after = ', string_representation)
+
+    return string_representation
