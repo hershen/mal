f9633bb5da29a69192c1ac2ef2e588bda739de79
Alon Hershenhorn <hershen@gmail.com>
2021-07-29 23:02:48 -0700

    Remove \\ before { and } in printer
    Remove the \\ charecters before the '{' and '}' in Strings in the
    printer function.

--- impls/myPython/printer.py
+++ impls/myPython/printer.py
@@ -6,12 +6,8 @@ def pr_str(mal_type, print_readably):
     # print(' before = ', string_representation, type(mal_type))
     if print_readably and isinstance(mal_type, mal_types.String):
         string_representation = '"' + string_representation[1:-1].replace('\\', '\\\\') \
-                                                                 .replace('"', '\\"')   \
-                                                                 .replace('{', '\\{')   \
-                                                                 .replace('}', '\\}')   \
+                                                                 .replace('"', '\\"') \
                               + '"'
-            #                                                  .replace(')', '\\)')   \
-            #                                                  .replace('"', '\\"')   
     # print(' after = ', string_representation)
 
     return string_representation
