76a35e21f3a4c053f0a4e5eed38bd3460a6236c9
Alon Hershenhorn <hershen@gmail.com>
2021-08-07 22:52:55 -0700

    Refactor add_escape_backslash function

--- impls/myPython/printer.py
+++ impls/myPython/printer.py
@@ -1,4 +1,9 @@
 import mal_types
+import reader
+
+def add_escape_backslash(input_string):
+    input_string_with_escape_chars = ['\\' + char if char in reader.slash_preceded_charecters else char for char in input_string[1:-1]]
+    return '"' + input_string_with_escape_chars + '"'
 
 def pr_str(mal_type, print_readably):
     string_representation = str(mal_type)
